b775259cdfe0b2ed73ae42023aa87a01bd9523be
brianlunch <blynch5@tcd.ie>
2020-03-13 14:10:25 +0000

    Updates

---
+++ precache-manifest.f2fb870d26273f54a962b2640bac03be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291f3fa1472a024860447375d3f77fc9",
+    "url": "/GHpagesTest/index.html"
+  },
+  {
+    "revision": "4b02ce7b0ce01b7bc898",
+    "url": "/GHpagesTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "644992cf89e1fb95a8d6",
+    "url": "/GHpagesTest/static/js/2.f8384d6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/GHpagesTest/static/js/2.f8384d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b02ce7b0ce01b7bc898",
+    "url": "/GHpagesTest/static/js/main.b5674a2e.chunk.js"
+  },
+  {
+    "revision": "5b34c2b05ac7880235eb",
+    "url": "/GHpagesTest/static/js/runtime-main.541ffbae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GHpagesTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
